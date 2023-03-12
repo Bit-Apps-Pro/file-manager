dddad7ec0d2f28287e71cc8b87ac0ba7051d2233
Bit-Code-Developer <developer@bitcode.pro>
2023-03-12 17:43:13 +0600

    wip: optimization

--- backend/app/Providers/FileManager/FileRoot.php
+++ backend/app/Providers/FileManager/FileRoot.php
@@ -2,7 +2,495 @@
 
 namespace BitApps\FM\FileManager;
 
-\defined('ABSPATH') or exit();
+\defined('ABSPATH') || exit();
+
+/**
+ * Provides Volume/Root options for elFInder
+ *
+ * @see https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options-2.1#root-options
+ */
 class FileRoot
 {
+    /**
+     * Volume driver. LocalFileSystem | MySQL | FTP
+     *
+     * @var string LocalFileSystem | MySQL | FTP
+     */
+    private $_driver;
+
+    /**
+     * Is support autoload.
+     *
+     * @var bool
+     */
+    private $_autoload;
+
+    /**
+     * Folder hash value on elFinder to be the parent of this volume (elFinder >= 2.1.15)
+     *
+     * @var string
+     */
+    private $_phash;
+
+    /**
+     * Folder hash value on elFinder to trash bin of this volume (elFinder >= 2.1.24)
+     * Folder hash value on elFinder to trash bin of this volume, it require 'copyJoin' to true
+     *
+     * @var string
+     */
+    private $_trashHash;
+
+    /**
+     * Root directory path
+     *
+     * @var string
+     */
+    private $_path;
+
+    /**
+     * Open this path on initial request instead of root path.
+     * Notice: In order to validate this option by a multi-route,
+     * you have to set a value only to the volume which applies this option.
+     *
+     * @var string
+     */
+    private $_startPath;
+
+    /**
+     * URL that points to path directory (also called 'root URL')
+     * If not set client will not see full path to files (replacement for old fileURL option),
+     * also all files downloads will be handled by connector.
+     *
+     * @var string
+     */
+    private $_URL;
+
+    /**
+     * This volume's local encoding.
+     * (server's file system encoding) It's necessary to be valid value to iconv.
+     *
+     * @see https://gist.github.com/nao-pon/78d64ad9a89d0267833564e5d61f0001
+     *
+     * @var string
+     */
+    private $_encoding;
+
+    /**
+     * This volume's local locale.
+     * It's important for encoding setting.
+     * It's necessary to be valid value in your server.
+     *
+     * Warning: Setting the locale correctly is very important.
+     * Especially if you do not match the character encoding to that of the server file system,
+     * it will create a security vulnerability.
+     *
+     * @var string
+     */
+    private $_locale;
+
+    /**
+     * Root path alias for volume root.
+     * If not set will use directory name of path.
+     *
+     * @var string
+     */
+    private $_alias;
+
+    /**
+     * Enable i18n folder name that convert name to elFinderInstance.messages['folder_'+name]
+     *
+     * @var bool
+     */
+    private $_i18nFolderName;
+
+    /**
+     * Method to detect files mime types.
+     *
+     * @var string auto | internal | finfo | mime_content_type
+     */
+    private $_mimeDetect;
+
+    /**
+     * Path to alternative mime types file. Only used when mimeDetect set to internal.
+     * If not set will use default mime.types
+     *
+     * @var string
+     */
+    private $_mimefile;
+
+    /**
+     * Additional MIME type normalize map
+     *
+     *      Example :
+     *  `array(
+     *   'tpl:application/vnd.groove-tool-template' => 'text/plain',
+     *  '[ext]:[Detected MIME Type]' => '[Normalized MIME Type]'
+     * )`
+     *
+     * @var array
+     */
+    private $_additionalMimeMap;
+
+    /**
+     * MIME regex of send HTTP header "Content-Disposition: inline"  on file open command.
+     * '.*' is allow inline of all of MIME types
+     *
+     * '$^' is not allow inline of all of MIME types
+     *
+     * '^(?:image|text/plain$)' is recommended for safety on public elFinder
+     *
+     * Default value: '^(?:(?:video|audio)|image/(?!.+\+xml)
+     * |application/(?:ogg|x-mpegURL|dash\+xml)|(?:text/plain|application/pdf)$)'
+     *
+     * @var string
+     */
+    private $_dispInlineRegex;
+
+    /**
+     * Image manipulations library
+     *
+     * Default value: 'auto'
+     *
+     * @var string auto | imagick | gd | convert
+     */
+    private $_imgLib;
+
+    /**
+     * Directory for thumbnails.
+     * If this is a simple filename, it will be prefixed with the root directory path.
+     * If you choose to use a location outside of the root directory,
+     * you should use a full pathname as a relative path using ellipses
+     * will get mangled and may not work (create thumbnails because tmbPath is NOT writable) on some server OS's.
+     *
+     * Default value: '.tmb'
+     *
+     * @var string
+     */
+    private $_tmbPath;
+
+    /**
+     * Umask for thumbnails dir creation.
+     *
+     * Default value: 0777
+     *
+     * @var octal
+     */
+    private $_tmbPathMode;
+
+    /**
+     * URL for thumbnails directory set in tmbPath.
+     * Set it only if you want to store thumbnails outside root directory.
+     * If you want chose original image as thumbnail it is able to set 'self'. (elFinder >= 2.1.12)
+     *
+     * @var string
+     */
+    private $_tmbURL;
+
+    /**
+     * Thumbnails size in pixels
+     *
+     * Default value: 48
+     *
+     * @var int
+     */
+    private $_tmbSize;
+
+    /**
+     * Crop thumbnails to fit tmbSize.
+     * true - resize and crop.
+     * false - scale image to fit thumbnail size.
+     *
+     * @var bool
+     */
+    private $_tmbCrop;
+
+    /**
+     * Thumbnails background color (hex #rrggbb or transparent)
+     *
+     * Default value: 'transparent'
+     *
+     * @var string
+     */
+    private $_tmbBgColor;
+
+    /**
+     * Image rotate fallback background color (hex #rrggbb).
+     * Uses this color if it can not specify to transparent.
+     *
+     * Default value: '#ffffff'
+     *
+     * @var bool
+     */
+    private $_bgColorFb;
+
+    /**
+     * Fallback self image to thumbnail (nothing imgLib)
+     *
+     * Default value: true
+     *
+     * @var bool
+     */
+    private $_tmbFbSelf;
+
+    /**
+     * Replace files on paste or give new names to pasted files.
+     * true - old file will be replaced with new one
+     * false - new file get name - original_name-number.ext
+     *
+     * Default value: true
+     *
+     * @var bool
+     */
+    private $_copyOverwrite;
+
+    /**
+     * Merge new and old content on paste
+     * true - join new and old directories content
+     * false - replace old directories with new ones
+     *
+     * Default value: true
+     *
+     * @var bool
+     */
+    private $_copyJoin;
+
+    /**
+     * Allow to copy from this volume to other ones.
+     *
+     * Default value: true
+     *
+     * @var true
+     */
+    private $_copyFrom;
+
+    /**
+     * Allow to copy from other volumes to this one
+     *
+     * Default value: true
+     *
+     * @var bool
+     */
+    private $_copyTo;
+
+    /**
+     * (temporary) Directory for extracts etc.
+     *
+     * @var string
+     */
+    private $_tmpPath;
+
+    /**
+     * Replace files with the same name on upload or give them new names
+     * true - replace old files
+     * false - give new names like original_name-number.ext
+     *
+     * Default value: true
+     *
+     * @var bool
+     */
+    private $_uploadOverwrite;
+
+    /**
+     * Mime types allowed to upload
+     * Default value: array()
+     * Example:
+     * 'uploadAllow' => array('image') # allow any images
+     * 'uploadAllow' => array('image/png', 'application/x-shockwave-flash') # allow png and flash
+     *
+     * @var array
+     */
+    private $_uploadAllow;
+
+    /**
+     * Mime types not allowed to upload.
+     * Same values accepted as in uploadAllow
+     *
+     * Default value: array()
+     *  Example:
+     *  'uploadDeny' => array('all') # deny of all types
+     *
+     * @var array
+     */
+    private $_uploadDeny;
+
+    /**
+     * Order to process uploadAllow and uploadDeny options.
+     * Logic is the same as Apache web server options Allow, Deny, Order
+     *
+     * Default value: array('deny', 'allow')
+     *
+     * @var array
+     */
+    private $_uploadOrder;
+
+    /**
+     * Maximum upload file size.
+     * Can be set as number or string with unit 10M, 500K, 1G.
+     * Note: elFinder 2.1+ support chunked file uploading.
+     * 0 means unlimited upload.
+     *
+     * @var int|string
+     */
+    private $_uploadMaxSize;
+
+    /**
+     * Maximum number of chunked upload connection, -1 to disable chunked upload.
+     *
+     * Default value: 3
+     *
+     * @var int
+     */
+    private $_uploadMaxConn;
+
+    /**
+     * Default file/directory permissions.
+     * Setting hidden, locked here - take no effect
+     *
+     * Default value: 'defaults' => array('read'  => true, 'write' => true)
+     *
+     * @var array
+     */
+    private $_defaults;
+
+    /**
+     * File permission attributes.
+     *
+     * @see https://github.com/Studio-42/elFinder/wiki/Simple-file-permissions-control
+     *
+     * @var array
+     */
+    private $_attributes;
+
+    /**
+     * Validate new file name regex or function
+     *
+     * Default value:Default value: '^[^\.].*'
+     *
+     * @var string|callable
+     */
+    private $_acceptedName;
+
+    /**
+     * Function or class instance method to control files permissions.
+     *
+     * @var callable|null
+     */
+    private $_accessControl;
+
+    /**
+     * Data that will be passed to access control method.
+     *
+     * @var mixed
+     */
+    private $_accessControlData;
+
+    /**
+     * List of commands disabled on this root.
+     *
+     * @see https://github.com/Studio-42/elFinder/wiki/Client-Server-API-2.1#command-list
+     *
+     * @var array
+     */
+    private $_disabled;
+
+    /**
+     * Include file owner, group & mode in stat results
+     *
+     * Default value: false
+     *
+     * @var bool
+     */
+    private $_statOwner;
+
+    /**
+     * Allow exec chmod of read-only files
+     *
+     * Default value: false
+     *
+     * @var bool
+     */
+    private $_allowChmodReadOnly;
+
+    /**
+     * How many sub dir levels return per request
+     *
+     * Default value: 1
+     *
+     * @var int
+     */
+    private $_treeDeep;
+
+    /**
+     * Check children directories for other directories in it.
+     * true - every folder will be check for children folders,
+     * -1 - every folder will be check asynchronously,
+     * false - all folders will be marked as having sub folders
+     *
+     * Default value: true
+     *
+     * @var bool|int
+     */
+    private $_checkSubfolders;
+
+    /**
+     * Directory separator
+     *
+     * Default value: DIRECTORY_SEPARATOR
+     *
+     * @var string
+     */
+    private $_separator;
+
+    /**
+     * File modification date format
+     *
+     * Default value: 'j M Y H:i'
+     *
+     * @var string
+     */
+    private $_dateFormat;
+
+    /**
+     * File modification time format
+     *
+     * Default value: 'H:i'
+     *
+     * @var string
+     */
+    private $_timeFormat;
+
+    /**
+     * Library to crypt/uncrypt files names (not implemented yet)
+     *
+     * @var [type]
+     */
+    private $_cryptLib;
+
+    /**
+     * Allowed archive's mime types to create
+     *
+     * @var array
+     */
+    private $_archiveMimes;
+
+    /**
+     * Manual config for archiver's.
+     *
+     * @var array
+     */
+    private $_archivers;
+
+    /**
+     * Temporary directory for extracting archives (LocalFileSystem volume only)
+     *
+     * @var string
+     */
+    private $_quarantine;
+
+    /**
+     * Configure plugin options of each volume
+     *
+     * @var array
+     */
+    private $_plugin;
 }
--- backend/app/Providers/FileManager/Options.php
+++ backend/app/Providers/FileManager/Options.php
@@ -2,7 +2,136 @@
 
 namespace BitApps\FM\FileManager;
 
-\defined('ABSPATH') or exit();
+\defined('ABSPATH') || exit();
 class Options
 {
+    /**
+     * Set locale. Currently only UTF-8 locales are supported. Passed to setLocale PHP function.
+     *
+     * Warning: Setting the locale correctly is very important.
+     *  Especially if you do not match the character encoding to that of the server file system,
+     *  it will create a security vulnerability.
+     *
+     * Default value: 'en_US.UTF-8'
+     *
+     * @var string
+     */
+    private $_locale;
+
+    /*
+     * elFinderVolumeDriver mime.type file path as defaults.
+     * This can be overridden in each of the volume by setting the volume root mimefile.
+     *  The default value '' meaning uses a file 'php/mime.type'.
+     */
+    private $_defaultMimefile;
+
+    /**
+     * Session handling wrapper class object.
+     * It must be implement elFinderSessionInterface.
+     *
+     * @var elFinderSessionInterface
+     */
+    private $_session;
+
+    /**
+     * Set sessionCacheKey. PHP $_SESSION array key of elFinder caches.
+     *
+     * Default value: 'elFinderCaches'
+     *
+     * @var string
+     */
+    private $_sessionCacheKey;
+
+    /**
+     * Finder save session data as UTF-8.
+     * If the session storage mechanism of the system does not allow UTF-8, and it must be set true.
+     *
+     * Default value: false
+     *
+     * @var string
+     * */
+    private $_base64encodeSessionData;
+
+    /**
+     * Temp directory path for Upload. Default uses sys_get_temp_dir()
+     *
+     * @var string
+     */
+    private $_uploadTempPath;
+
+    /*
+     * Temp directory path for temporally working files. Default uses ./.tmp if it writable.
+     *
+     * Default value: './.tmp' or sys_get_temp_dir()
+     */
+    private $_commonTempPath;
+
+    /**
+     * Connection flag files path that connection check of current request.
+     *  A file is created every time an access is made to this location and it is deleted at the end of the request.
+     * It is recommended to specify RAM disk such as "/dev/shm".
+     *
+     * Default value: commonTempPath or ''
+     *
+     * @var string
+     */
+    private $_connectionFlagsPath;
+
+    /**
+     * Max allowed archive files size (0 - no limit)
+     * Default value: 0
+     *
+     * @var int
+     */
+    private $_maxArcFilesSize;
+
+    /**
+     * Root options of the network mounting volume
+     * Default value: array()
+     * Example:
+     * 'optionsNetVolumes' => array(
+     *    // key '*' is common additional volume root options
+     *    '*'   => array(),
+     *    // key of elFinder::$netDrivers is each protocol volumes
+     *    'ftp' => array()
+     *  )
+     *
+     * @var array
+     */
+    private $_optionsNetVolumes;
+
+    /**
+     * Max number of limits of selectable items (0 - no limit)
+     *
+     * Default value: 1000
+     *
+     * @var int
+     */
+    private $_maxTargets;
+
+    /*
+     * Throw Error on exec()
+     * true need try{} block for $connector->run();
+     *
+     * Default value: false
+     */
+    private $_throwErrorOnExec;
+
+    /**
+     * Send debug to client.
+     *
+     * Default value: false
+     *
+     * @var bool
+     */
+    private $_debug;
+
+    /**
+     * Bind callbacks for user actions
+     *
+     * @see https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options-2.1#bind
+     *
+     * @var array<string, callable>
+     */
+    private $_bind;
 }
