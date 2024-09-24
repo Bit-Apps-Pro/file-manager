0a86b1c81d0ef115df4c0614e92f7bfc6989fe8c
Shakhawat Hosen <mdshakhawathosen122@gmail.com>
2024-09-24 11:34:21 +0600

    chore: separate file created for changeLogs data

---
+++ frontend/src/changeLog.ts
@@ -0,0 +1,10 @@
+const changeLogs = {
+  improvements: ['UI updated.', 'Multiple drive/folder by user/role.', 'ElFinder updated.'],
+  fixed: [
+    'Addressed a potential RCE vulnerability related to a race condition during PHP syntax checks.',
+    'Restricted file types in shortcodes. Users must grant permission to allow PHP files.',
+    'PDF preview due to wrong file path.'
+  ]
+}
+
+export default changeLogs
