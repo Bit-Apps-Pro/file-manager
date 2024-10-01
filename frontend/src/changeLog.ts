cdaf90467863e9cf49e806e7eeb4f3c6af72cde5
Shakhawat Hosen <mdshakhawathosen122@gmail.com>
2024-10-01 16:39:25 +0600

    chore: bit social release menu button and modal added, telemetry popup modified

--- frontend/src/changeLog.ts
+++ frontend/src/changeLog.ts
@@ -1,8 +1,7 @@
 const changeLogs = {
-  improvements: ['UI updated.', 'Multiple drive/folder by user/role.', 'ElFinder updated.'],
+  improvements: ['UI updated.', 'Multiple drive/folder by user/role.'],
   fixed: [
     'Addressed a potential RCE vulnerability related to a race condition during PHP syntax checks.',
-    'Restricted file types in shortcodes. Users must grant permission to allow PHP files.',
     'PDF preview due to wrong file path.'
   ]
 }
